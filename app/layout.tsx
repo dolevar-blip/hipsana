7a42d8001c5171b98de978d3562a2b43008c5589
dolevar-blip <dolevar@gmail.com>
2026-06-19 07:54:50 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,6 @@ const ibmPlexSans = IBM_Plex_Sans({
 });
 
 const GA_MEASUREMENT_ID = "G-9SDHNCV0H0";
-const CLARITY_PROJECT_ID = "x0hl18yf1z";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://hipsana.com"),
@@ -103,15 +102,6 @@ export default function RootLayout({
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', '${GA_MEASUREMENT_ID}');
-          `}
-        </Script>
-        <Script id="microsoft-clarity" strategy="afterInteractive">
-          {`
-            (function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "${CLARITY_PROJECT_ID}");
           `}
         </Script>
         <Header />
